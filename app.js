27cf7474ffee83fddc035ecd7c032492c65a071c
Felix <166325423+felixtrom@users.noreply.github.com>
2026-07-07 15:57:49 -0600

    Initialize app and add screen navigation

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Tonight's Pick has started.");
+const app = document.getElementById("app");
+console.log(app);
+let currentScreen = "landing";
+
+function showScreen(screenName) {
+    currentScreen = screenName;
+    console.log("Current screen:", currentScreen);
+}
+
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", () => {
+    showScreen("question");
+});
