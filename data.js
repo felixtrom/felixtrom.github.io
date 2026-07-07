6a3f25aa793cc19cb83419a2b190aa316fb5018a
Felix <166325423+felixtrom@users.noreply.github.com>
2026-07-07 14:46:49 -0600

    Create tree structure for movie recommendation
    Added a tree structure with questions and answers for movie recommendations.

--- data.js
+++ data.js
@@ -1,1 +1,83 @@
+const tree = {
 
+    question:"What sounds best tonight?",
+
+    answers:[
+
+        {
+
+            text:"Something Light",
+
+            next:{
+
+                question:"What kind of light?",
+
+                answers:[
+
+                    {
+                        text:"Comedy",
+                        result:"Game Night"
+                    },
+
+                    {
+                        text:"Adventure",
+                        result:"The Secret Life of Walter Mitty"
+                    }
+
+                ]
+
+            }
+
+        },
+
+        {
+
+            text:"Something Romantic",
+
+            next:{
+
+                question:"Happy or emotional?",
+
+                answers:[
+
+                    {
+
+                        text:"Happy",
+
+                        result:"About Time"
+
+                    },
+
+                    {
+
+                        text:"Emotional",
+
+                        result:"Past Lives"
+
+                    }
+
+                ]
+
+            }
+
+        },
+
+        {
+
+            text:"Something Curious",
+
+            result:"Arrival"
+
+        },
+
+        {
+
+            text:"Something Thought Provoking",
+
+            result:"The Prestige"
+
+        }
+
+    ]
+
+};
